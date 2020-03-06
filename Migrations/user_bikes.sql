b477d59f00e173f6c0c663ba1622c32baf9f740c
NoahNovick <nnovickfaille1@gmail.com>
2020-03-06 14:29:19 -0500

    sql seed files populated

--- Migrations/user_bikes.sql
+++ Migrations/user_bikes.sql
@@ -1,7 +1,6 @@
 CREATE TABLE users(
   id SERIAL PRIMARY KEY,
-  email VARCHAR(255) NOT NULL,
-  password VARCHAR (255) NOT NULL
+  email VARCHAR(255) NOT NULL
 )
 
 CREATE TABLE bikes(
---
+++ seeds/users.sql
@@ -0,0 +1,7 @@
+INSERT INTO users (email) values
+(nnovickfaille1@gmail.com),
+(noah@landlab.ca),
+(nnovickfaille@gmail.com),
+(kevin.m.guertin@gmail.com),
+(david_he613@gmail.com),
+(david.he07@gmail.com)
