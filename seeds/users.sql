dff4a8f9e3405d567dfb2e91745d9700bd044eb3
NoahNovick <nnovickfaille1@gmail.com>
2020-03-06 14:51:45 -0500

    database fill with bikes and users

--- seeds/users.sql
+++ seeds/users.sql
@@ -1,7 +1,6 @@
-INSERT INTO users (email) values
-(nnovickfaille1@gmail.com),
-(noah@landlab.ca),
-(nnovickfaille@gmail.com),
-(kevin.m.guertin@gmail.com),
-(david_he613@gmail.com),
-(david.he07@gmail.com)
+INSERT INTO users (email) values('nnovickfaille1@gmail.com'),
+('noah@landlab.ca'),
+('nnovickfaille@gmail.com'),
+('kevin.m.guertin@gmail.com'),
+('david_he613@gmail.com'),
+('david.he07@gmail.com');
