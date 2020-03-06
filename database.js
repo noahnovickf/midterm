8777923c336cde621893567c8374a557cec45bab
NoahNovick <nnovickfaille1@gmail.com>
2020-03-06 17:22:56 -0500

    database and seeds add

---
+++ database.js
@@ -0,0 +1,86 @@
+import { Pool } from "pg";
+
+const pool = new Pool({
+  user: "user",
+  password: "123",
+  host: "localhost",
+  database: "midterm"
+});
+
+const addListing = bike => {
+  return pool
+    .query(
+      `INSERT INTO bikes (user_id, title, description, image_url, price, category, discipline, featured)
+    Values ($1,$2,$3,$4,$5,$6,$7,false)
+    RETURNING *;
+    `,
+      [
+        bike.user_id,
+        bike.title,
+        bike.description,
+        bike.image_url,
+        bike.price,
+        bike.category,
+        bike.discipline,
+        bike.featured
+      ]
+    )
+    .then(res => res.rows[0]);
+};
+
+const getFeaturedBikes = () => {
+  return pool
+    .query(
+      `
+  SELECT * FROM bikes WHERE featured = true
+  `
+    )
+    .then(res => res.rows);
+};
+
+const getAllBikes = () => {
+  return pool
+    .query(
+      `
+  SELECT * FROM bikes;
+  `
+    )
+    .then(res => res.rows);
+};
+
+const filterBikes = options => {
+  const queryParams = [];
+  let queryString = `SELECT * FROM bikes WHERE 1=1`;
+  if (options.minimum_price) {
+    queryParams.push(Number(options.minimum_price));
+    queryString += `AND price >$${queryParams.length}`;
+  }
+  if (options.maximum_price) {
+    queryParams.push(Number(options.maximum_price));
+    queryString += `AND price <$${queryParams.length}`;
+  }
+
+  return pool.query(`
+  `);
+};
+
+const favouriteBike = ids => {
+  return pool.query(
+    `INSERT INTO favourites (user_id, bike_id)
+    VALUES ($1,$2)
+    RETURNING *;
+    `,
+    [ids.user_id, ids.bike_id]
+  );
+};
+
+const getFavouriteBikes = user => {
+  return pool
+    .query(
+      `SELECT * from favourites
+    WHERE user_id = $1
+    `,
+      [user.user_id]
+    )
+    .then(res => res.rows);
+};
