4cedc367f504bb9161c68d17bf300caea3a6af78
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-03-09 15:42:27 +0000

    add event handlers to page buttons

---
+++ public/js/eventHandler.js
@@ -0,0 +1,36 @@
+// Load Event Handlers On Page Load
+$(() =>{
+
+  $('.favourites-btn').on('click', () => {
+    console.log('hi')
+  });
+
+  $('.messages-btn').on('click', () => {
+    console.log('hi')
+  });
+
+  $('.post-ad-btn').on('click', () => {
+    console.log('hi')
+  });
+
+  $('.login-btn').on('click', () => {
+    console.log('hi')
+  });
+
+  $('.dropdown-road').on('click', () => {
+    console.log('hi')
+  });
+
+  $('.dropdown-mountain').on('click', () => {
+    console.log('hi')
+  });
+
+  $('.dropdown-other').on('click', () => {
+    console.log('hi')
+  });
+
+  $('.dropdown-other').on('click', () => {
+    console.log('hi')
+  });
+
+});
