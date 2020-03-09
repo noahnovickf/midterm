a11869c994606940b8135341f3fadaa6eed4b7f6
NoahNovick <nnovickfaille1@gmail.com>
2020-03-09 15:32:13 -0400

    add event handlers and start login

---
+++ public/js/event-handlers/filtertype.js
@@ -0,0 +1,104 @@
+$(() => {
+  $(".dropdown-road").on("click", e => {
+    e.preventDefault();
+    console.log("bye");
+
+    // const featuredBikes = $;
+    $.ajax({ url: "/api", method: "GET" }).then(res => {
+      return renderBikes(res);
+    });
+
+    const renderBikes = res => {
+      $("#bikeDisplay").empty();
+      for (let bike of res.bikes) {
+        if (bike.category === "road")
+          $("#bikeDisplay").prepend(createBikeCard(bike));
+      }
+    };
+    const createBikeCard = bike => {
+      //console.log(bike.image_url);
+      let card = $('<div class="card">');
+      const html = `    <img src='${bike.image_url}' class="card-img-top" alt="Bike image" />
+  <div class="card-body">
+  <h5 class="card-title">${bike.title}</h5>
+    <h6>$${bike.price}</h6>
+    <p class="card-text">
+    ${bike.description}
+    </p>
+    <a href="#" class="btn btn-primary">Favourite</a>
+    <a href="#" class="btn btn-primary">Contact Seller</a>
+    </div>`;
+      card.append(html);
+      return card;
+    };
+    $(".all-bikes-btn").css({ display: "inline-block" });
+  });
+  $(".dropdown-mountain").on("click", e => {
+    e.preventDefault();
+    console.log("bye");
+
+    // const featuredBikes = $;
+    $.ajax({ url: "/api", method: "GET" }).then(res => {
+      return renderBikes(res);
+    });
+
+    const renderBikes = res => {
+      $("#bikeDisplay").empty();
+      for (let bike of res.bikes) {
+        if (bike.category === "mountain")
+          $("#bikeDisplay").prepend(createBikeCard(bike));
+      }
+    };
+    const createBikeCard = bike => {
+      //console.log(bike.image_url);
+      let card = $('<div class="card">');
+      const html = `    <img src='${bike.image_url}' class="card-img-top" alt="Bike image" />
+  <div class="card-body">
+  <h5 class="card-title">${bike.title}</h5>
+    <h6>$${bike.price}</h6>
+    <p class="card-text">
+    ${bike.description}
+    </p>
+    <a href="#" class="btn btn-primary">Favourite</a>
+    <a href="#" class="btn btn-primary">Contact Seller</a>
+    </div>`;
+      card.append(html);
+      return card;
+    };
+    $(".all-bikes-btn").css({ display: "inline-block" });
+  });
+  $(".dropdown-other").on("click", e => {
+    e.preventDefault();
+    console.log("bye");
+
+    // const featuredBikes = $;
+    $.ajax({ url: "/api", method: "GET" }).then(res => {
+      return renderBikes(res);
+    });
+
+    const renderBikes = res => {
+      $("#bikeDisplay").empty();
+      for (let bike of res.bikes) {
+        if (bike.category === "other")
+          $("#bikeDisplay").prepend(createBikeCard(bike));
+      }
+    };
+    const createBikeCard = bike => {
+      //console.log(bike.image_url);
+      let card = $('<div class="card">');
+      const html = `    <img src='${bike.image_url}' class="card-img-top" alt="Bike image" />
+  <div class="card-body">
+  <h5 class="card-title">${bike.title}</h5>
+    <h6>$${bike.price}</h6>
+    <p class="card-text">
+    ${bike.description}
+    </p>
+    <a href="#" class="btn btn-primary">Favourite</a>
+    <a href="#" class="btn btn-primary">Contact Seller</a>
+    </div>`;
+      card.append(html);
+      return card;
+    };
+    $(".all-bikes-btn").css({ display: "inline-block" });
+  });
+});
--- public/js/eventHandler.js
+++ public/js/eventHandler.js
@@ -1,40 +1,14 @@
 // Load Event Handlers On Page Load
-$(() =>{
-
-  $('.favourites-btn').on('click', () => {
-    console.log('hi')
+$(() => {
+  $(".favourites-btn").on("click", () => {
+    console.log("hi");
   });
 
-  $('.messages-btn').on('click', () => {
-    console.log('hi')
+  $(".messages-btn").on("click", () => {
+    console.log("hi");
   });
 
-  $('.post-ad-btn').on('click', () => {
-    console.log('hi')
+  $(".post-ad-btn").on("click", () => {
+    console.log("hi");
   });
-
-  $('.login-btn').on('click', () => {
-    console.log('hi')
-  });
-
-  $('.dropdown-road').on('click', (e) => {
-    e.preventDefault();
-    console.log('hi');
-  });
-
-  $('.dropdown-mountain').on('click', (e) => {
-    e.preventDefault();
-    console.log('hi');
-  });
-
-  $('.dropdown-other').on('click', (e) => {
-    e.preventDefault();
-    console.log('hi');
-  });
-
-  $('.all-bikes-btn').on('click', (e) => {
-    e.preventDefault();
-    console.log('bye');
-  })
-
 });
