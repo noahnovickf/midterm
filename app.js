fd9e8cbb40837b2e4666a28518f30ceb42a11ca6
NoahNovick <nnovickfaille1@gmail.com>
2020-03-09 17:07:31 -0400

    login form and cookies working

--- app.js
+++ app.js
@@ -2,9 +2,11 @@ const express = require("express");
 const app = express();
 const port = 3000;
 const db = require("./database");
+const bodyParser = require("body-parser");
 
 const apiRouter = require("./routes/apiRoutes");
 
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/api", apiRouter(db));
 app.use("/allbikes", apiRouter(db));
 app.use(express.static("public"));
