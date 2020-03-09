28d9cd05543927eef0e6361b5ae8a2cb5e920c26
David <david.he07@yahoo.ca>
2020-03-09 13:34:43 -0400

    changed html files

--- sql/seeds/users.sql
+++ sql/seeds/users.sql
@@ -3,7 +3,8 @@ INSERT INTO users (email) values('nnovickfaille1@gmail.com'),
 ('nnovickfaille@gmail.com'),
 ('kevin.m.guertin@gmail.com'),
 ('david_he613@gmail.com'),
-('david.he07@gmail.com');
+('david.he07@gmail.com'),
+('kguertin90@gmail.com');
 
 
-INSERT INTO favourites(user_id, bike_id) values ('6','55')\q
+-- INSERT INTO favourites(user_id, bike_id) values ('6','55')\q
