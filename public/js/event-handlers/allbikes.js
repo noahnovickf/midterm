58b36ed63ec1a56f05fab9679a89a6e2a2935b9c
NoahNovick <nnovickfaille1@gmail.com>
2020-03-10 18:59:14 -0400

    sold done and delete

--- public/js/event-handlers/allbikes.js
+++ public/js/event-handlers/allbikes.js
@@ -33,9 +33,17 @@ $(() => {
       })
       .then(() => {
         $(".sold-btn").on("click", e => {
-          console.log("works");
+          $.ajax({
+            url: "/api/sold",
+            method: "POST",
+            dataType: "json",
+            data: {
+              bike_id: $(e.currentTarget).data("id")
+            }
+          });
         });
       })
+
       .then(() => {
         if (document.cookie.slice(9) === "noah%40landlab.ca") {
           $(".admin-btns").css({ display: "inline" });
@@ -52,20 +60,29 @@ $(() => {
     };
     const createBikeCard = bike => {
       let card = $('<div class="card">');
-      const html = `    <img src='${bike.image_url}' class="card-img-top" alt="Bike image" />
-  <div class="card-body">
+      const html = `    <img src='${bike.image_url}' class="card-img-top${
+        bike.sold ? " grey" : ""
+      }" alt="Bike image" />
+      <div class="card-body ">
+      
   <h5 class="card-title">${bike.title}</h5>
-    <h6>$${bike.price}</h6>
+    <h6>${bike.sold ? `<span class='sold'>SOLD</span>` : `$${bike.price}`}</h6>
     <p class="card-text">
     ${bike.description}
     </p>
     <div class="user-btns">
-    <button data-id="${bike.id}" class="btn btn-primary add-fav-btn">Favourite</button>
+    <button data-id="${
+      bike.id
+    }" class="btn btn-primary add-fav-btn">Favourite</button>
     <button  class="btn btn-primary">Contact Seller</button>
     </div>
     <div class="admin-btns">
-    <button data-id="${bike.id}" class="btn btn-danger delete-btn">Delete</button>
-    <button class="btn btn-danger sold-btn">Mark Sold</button>
+    <button data-id="${
+      bike.id
+    }" class="btn btn-danger delete-btn">Delete</button>
+    <button data-id="${
+      bike.id
+    }" class="btn btn-danger sold-btn">Mark Sold</button>
     </div>
     </div>`;
       card.append(html);
