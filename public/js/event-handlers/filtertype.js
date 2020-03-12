6437a61bae2ef5c0386966bc16a9291a4236582f
NoahNovick <nnovickfaille1@gmail.com>
2020-03-12 11:46:49 -0400

    empty addbike form

--- public/js/event-handlers/filtertype.js
+++ public/js/event-handlers/filtertype.js
@@ -16,9 +16,7 @@ $(() => {
     console.log("success");
     $.ajax({ url: "/api", method: "GET" })
       .then(res => {
-        //console.log(res.bikes[0].category);
         let filterBike = res.bikes.filter(bike => {
-          //console.log(bike);
           return bike.category === road;
         });
         renderBikes(filterBike);
@@ -31,46 +29,4 @@ $(() => {
         }
       });
   };
-
-  const renderBikes = res => {
-    $("#bikeDisplay").empty();
-    for (let bike of res) {
-      $("#bikeDisplay").prepend(createBikeCard(bike));
-    }
-  };
-
-  const createBikeCard = bike => {
-    let card = $('<div class="card">');
-    const html = `    <img src='${bike.image_url}' class="card-img-top${
-      bike.sold ? " grey" : ""
-    }" alt="Bike image" />
-        <div class="card-body ">
-        
-    <h5 class="card-title">${bike.title}</h5>
-      <h6>${
-        bike.sold ? `<span class='sold'>SOLD</span>` : `$${bike.price}`
-      }</h6>
-      <p class="card-text">
-      ${bike.description}
-      </p>
-      <div class="user-btns">
-      <button data-id="${
-        bike.id
-      }" class="btn btn-primary add-fav-btn">Favourite</button>
-      <button  class="btn btn-primary">Contact Seller</button>
-      </div>
-      <div class="admin-btns">
-      <button data-id="${
-        bike.id
-      }" class="btn btn-danger delete-btn">Delete</button>
-      <button data-id="${
-        bike.id
-      }" class="btn btn-danger sold-btn">Mark Sold</button>
-      </div>
-      </div>`;
-    card.append(html);
-    return card;
-  };
-
-  $(".all-bikes-btn").css({ display: "inline-block" });
 });
