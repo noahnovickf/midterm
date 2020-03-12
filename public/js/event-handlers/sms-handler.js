c9a9af5c86fc3c03653683196f4de025148af2a3
Kevin Guertin <kevin.m.guertin@gmail.com>
2020-03-12 18:22:03 +0000

    add send sms server logic

--- public/js/event-handlers/sms-handler.js
+++ public/js/event-handlers/sms-handler.js
@@ -1,11 +1,12 @@
 $(() => {
   $('.send-sms-btn').on('click', () => {
+    console.log('hello from the button');
     $.ajax({
-      url: 'api/sendsms',
+      url: '/api/sms/send',
       method: 'POST',
       dataType: 'json',
       data: { message: 'Your Bike has been sold!'}
-  })
+    })
   })
 });
 
---
+++ testsms.js
@@ -0,0 +1,11 @@
+const ID = "AC4a51272f6caea6b1d6f01820179b143f";
+const token = "efdabc674c0e61b7e27ffab24d348598";
+const client = require("twilio")(ID, token);
+client.messages
+  .create({
+    body: "You just sent received a message about the bike you are selling!",
+    from: "+16364775721",
+    to: "+12899271833"
+  })
+  .then(message => console.log(message.sid))
+  .catch(err=>console.log(err))
